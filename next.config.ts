d6b8eb72ddb6a598d8a606f7d8a722a04ba437fd
sean Dittmar <coder001000@gmail.com>
2025-10-20 14:25:59 -0700

    op

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
