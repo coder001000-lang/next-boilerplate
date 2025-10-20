44f70a66286c42fb20d26fc4636b40c79f72f7ef
sean Dittmar <coder001000@gmail.com>
2025-10-20 08:04:53 -0700

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import React from 'react'
+import "./globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
